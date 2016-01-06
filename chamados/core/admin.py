8495949b2b8c0e8070b85741d57bef4cfaf0896c
Willian <williangd@yahoo.com.br>
2016-01-06 11:01:57 -0200

    Alterações no model e admin

--- chamados/core/admin.py
+++ chamados/core/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.utils.timezone import now
 from .models import Solicitante, Empresa, Status, Chamado
+
+class ChamadoModelAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'status', 'empresa', 'solicitante', 'abertura', 'fechamento')
+    date_hierarchy = 'abertura'
+    search_fields = ('titulo',)
+    list_filter = ('abertura',)     
+
 
 admin.site.register(Solicitante)
 admin.site.register(Empresa)
 admin.site.register(Status)
-admin.site.register(Chamado)
+admin.site.register(Chamado, ChamadoModelAdmin)
--- chamados/core/models.py
+++ chamados/core/models.py
@@ -3,21 +3,38 @@ from django.db import models
 class Solicitante(models.Model):
     nome = models.CharField(max_length=100)
 #    default = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome
 
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.nome
+
 
 class Status(models.Model):
     nome = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
 
 
 class Chamado(models.Model):
-    abertura = models.DateTimeField()
-    fechamento = models.DateTimeField(blank=True, null=True)
+    abertura = models.DateTimeField('abertura',)
+    fechamento = models.DateTimeField('fechamento', blank=True, null=True)
     solicitante = models.ForeignKey(Solicitante, on_delete=models.CASCADE, null=True)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, null=True)
     status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
-    problema = models.TextField(blank=True, null=True)
+    titulo = models.CharField('título', blank=True, null=True, max_length=200)
+    problema = models.TextField('problema', blank=True, null=True)
+
+
+    class Meta:
+            verbose_name_plural = 'chamados'
+            verbose_name = 'chamado'
+            ordering = ('-abertura',)
+
+    #def __str__(self):
+    #    return self.titulo
